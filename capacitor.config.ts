b21c60d60967d63d29fcb509c3d0861833be6a3f
LauraOvalle272 <125744566+LauraOvalle272@users.noreply.github.com>
2023-09-20 15:11:42 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projectApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
